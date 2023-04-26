bcfc0d3b77233b7a2ae6404f61c2f68a41067903
itsC-Moon <hichambenoukaiss0@gmail.com>
2023-04-26 09:12:22 +0100

    auto

--- outils.c
+++ outils.c
@@ -21,12 +21,12 @@ int _intlen(int n)
 {
 	int i = 0;
 
-	if (n < 0)
-
-		while (n != 0)
-		{
-			n /= 10;
-			i++;
-		}
+	if (n <= 0)
+		i++;
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
 	return (i);
 }
